a44e3347621a33f1f9107028530166deabca21a9
lgtm <1gtm@users.noreply.github.com>
2021-05-03 19:23:33 -0700

    Update Kubernetes toolchain to v1.21.0 (#195)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -29,7 +29,6 @@ import (
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
-	"kmodules.xyz/client-go/meta"
 	"kmodules.xyz/client-go/tools/clientcmd"
 )
 
@@ -49,7 +48,6 @@ func NewPerconaXtraDBServerOptions(out, errOut io.Writer) *PerconaXtraDBServerOp
 		RecommendedOptions: genericoptions.NewRecommendedOptions(
 			defaultEtcdPathPrefix,
 			server.Codecs.LegacyCodec(admissionv1beta1.SchemeGroupVersion),
-			genericoptions.NewProcessInfo("percona-xtradb-operator", meta.Namespace()),
 		),
 		ExtraOptions: NewExtraOptions(),
 		StdOut:       out,
